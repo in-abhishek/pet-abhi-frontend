e817cdddaaa5c689567b8e77f0c38310c054b215
Abhishek kumar <98520990+in-abhishek@users.noreply.github.com>
2026-02-19 12:52:31 +0530

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   const navigate = useNavigate()
   const { execute: logoutExecute } = usePost<void, any>("/logout");
   const handleLogout = () => {
-    logoutExecute(null, {
+    logoutExecute(undefined, {
       credentials: "include",
       onSuccess: () => {
         setAccessToken("");
